15e3d26be7a45422d049d897d2f76e242bc4a927
justbeay <robert.li@supernovacompanies.biz>
2018-06-13 15:19:59 +0800

    update

--- event_handler.py
+++ event_handler.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 
 import json
-import os
 import re
 import urllib
-from requests.auth import HTTPBasicAuth
 import ssl
-import hmac
-from hashlib import sha1
 
 class GithubEventHandler:
     def __init__(self, app, event, data):
@@ -28,16 +24,21 @@ class GithubEventHandler:
         if self.event == 'ping':
             return {'msg': 'Hi!'}
         else:
-            event_hit = False
+            task_match = []
             repo_config = self.get_repo_config()
-            if self.event == 'push':
-                event_hit = self._is_push_hit(repo_config)
-            elif self.event == 'pull_request':
-                event_hit = self._is_pull_request_hit(repo_config)
+            if repo_config:
+                for task_config in repo_config['tasks']:
+                    event_hit = False
+                    if self.event == 'push':
+                        event_hit = self._is_task_push(task_config)
+                    elif self.event == 'pull_request':
+                        event_hit = self._is_task_pull_request(task_config)
+                    if event_hit:
+                        task_match.append(task_config)
             # work start execute here...
-            if repo_config and event_hit:
+            for task in task_match:
                 self.app.logger.debug("event hit, start tasks under %s/%s...", self.repo_meta['owner'], self.repo_meta['name'])
-                self._jenkins_build(repo_config)
+                self._jenkins_build(task)
                 pass
             return "OK"
 
@@ -47,10 +48,12 @@ class GithubEventHandler:
                 'name': self.data['repository']['name'],
                 'owner': self.data['repository']['owner']['login'],
             }
+            if 'sender' in self.data:
+                self.repo_meta['sender'] = self.data['sender']['login']
             return self.repos_config.get('{owner}/{name}'.format(**self.repo_meta), None)
         return None
 
-    def _is_push_hit(self, repo_config):
+    def _is_task_push(self, task_config):
         '''
         determine whether this event hits the corresponding repos
         '''
@@ -58,35 +61,59 @@ class GithubEventHandler:
         match = re.match(r"refs/heads/(?P<branch>.*)", self.data['ref'])
         if match:
             self.repo_meta['branch'] = match.groupdict()['branch']
-            if repo_config and (not repo_config['branch'] or self.repo_meta['branch'] in repo_config['branch']):
-                if not repo_config['event'] or 'pull_request' in repo_config['event']:
-                    if self.data['commits'][-1]['message'].startswith('Merge pull request'):
-                        return False
-                return True
+            # for task in repo_config['tasks']:
+            if not ('branch' in task_config and self.repo_meta['branch'] not in task_config['branch'] or 'push' not in task_config['event']):
+                hit_flag = False
+                self.repo_meta['commit_ids'] = []
+                for commit in self.data['commits']:
+                    if not ('message_pattern' in task_config and not re.match(task_config['message_pattern'], commit['message'])):
+                        hit_flag = True
+                    self.repo_meta['commit_ids'].append(commit['id'])
+                if hit_flag:
+                    self.app.logger.info('push commit: [%s] to %s/%s:%s', ','.join(self.repo_meta['commit_ids']),
+                                self.repo_meta['owner'], self.repo_meta['name'], self.repo_meta['branch'])
+                    return True
         return False
 
-    def _is_pull_request_hit(self, repo_config):
+    def _is_task_pull_request(self, task_config):
         self.repo_meta['action'] = self.data['action']
         self.repo_meta['branch'] = self.data['pull_request']['base']['ref']
         self.repo_meta['pull_request'] = {
+            'number': self.data['number'],
             'branch': self.data['pull_request']['head']['ref'],
             'name': self.data['pull_request']['head']['repo']['name'],
             'owner': self.data['pull_request']['head']['repo']['owner']['login'],
-            'created_by': self.data['pull_request']['user']['login']
+            'created_by': self.data['pull_request']['user']['login'],
+            'author': {'email': self._get_pull_author(self.data['number'])}
         }
-        if repo_config and (not repo_config['branch'] or self.repo_meta['branch'] in repo_config['branch']):
-            if self.data['pull_request']['merged']:
-                self.repo_meta['pull_request']['merged_by'] = self.data['pull_request']['merged_by']['login']
-            self.app.logger.info('%s %s\'s pull request(%s/%s:%s => %s/%s:%s), merged: %s',
-                        self.repo_meta['action'], self.repo_meta['pull_request']['created_by'],
-                        self.repo_meta['pull_request']['owner'], self.repo_meta['pull_request']['name'],
-                        self.repo_meta['pull_request']['branch'], self.repo_meta['owner'],
-                        self.repo_meta['name'], self.repo_meta['branch'],
-                        self.data['pull_request']['merged'])
-            return 'merged_by' in self.repo_meta['pull_request']
+        if not ('branch' in task_config and self.repo_meta['branch'] not in task_config['branch'] or 'pull_request' not in task_config['event']):
+            if not ('action' in task_config and self.data['action'] not in task_config['action']):
+                if not ('merged' in task_config and task_config['merged'] != self.data['pull_request']['merged']):
+                    # task hit
+                    if self.data['pull_request']['merged']:
+                        self.repo_meta['pull_request']['merged_by'] = self.data['pull_request']['merged_by']['login']
+                    self.app.logger.info('%s %s\'s %s pull request (%s/%s:%s => %s/%s:%s), number:%d',
+                                self.repo_meta['action'], self.repo_meta['pull_request']['created_by'],
+                                'merged' if self.data['pull_request']['merged'] else 'unmerged',
+                                self.repo_meta['pull_request']['owner'], self.repo_meta['pull_request']['name'],
+                                self.repo_meta['pull_request']['branch'], self.repo_meta['owner'],
+                                self.repo_meta['name'], self.repo_meta['branch'], self.repo_meta['pull_request']['number'])
+                    return True
         return False
 
-    def _jenkins_build(self, repo_config):
+    def _get_pull_author(self, pull_id):
+        ssl._create_default_https_context = ssl._create_unverified_context
+        opener = urllib.request.build_opener()
+        urllib.request.install_opener(opener)
+        github_api_url = 'https://api.github.com/repos/justbeay/jenkinsci-test/pulls/%s/commits' % pull_id
+        req = urllib.request.Request(url=github_api_url, method='GET')
+        res = opener.open(req)
+        json_result = json.loads(res.read().decode('utf-8'))
+        res.close()
+        email = json_result[-1]['commit']['author']['email'] if json_result and len(json_result) > 0 else None
+        return email
+
+    def _jenkins_build(self, task_config):
         passman = urllib.request.HTTPPasswordMgrWithPriorAuth()
         passman.add_password(None,
                              self.global_config['jenkins']['host'],
@@ -99,8 +126,8 @@ class GithubEventHandler:
         # opener.add_handler(urllib.request.ProxyHandler(dict(http='http://127.0.0.1:5555')))
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         urllib.request.install_opener(opener)
-        for build_config in repo_config['jenkins_build']:
-            self.app.logger.info('>> start jenkins build job: %s...', build_config['job'])
+        for build_config in task_config['jenkins_build']:
+            self.app.logger.info('>> execute jenkins build job: %s...', build_config['job'])
             # get build api url
             build_api_url = self.global_config['jenkins']['host']
             for project_name in build_config['job'].split('/'):
